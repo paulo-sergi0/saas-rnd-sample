b19dbf5b10958fa20975cd1ab407e45bdba236b9
paulo-sergi0 <paulo-sergi0@hotmail.com>
2024-11-09 11:14:58 -0300

    Prepare for Railway

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -24,8 +24,15 @@ SECRET_KEY = 'django-insecure-&lbu55wro&5xhuzls!ghde1wy8gaa)6xvepn@@x79)2s2q8glo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+        ".raiway.app" #https://saas.prod.rails.app
+]
 
+if DEBUG:
+    ALLOWED_HOSTS  += [
+        "127.0.0.1",
+        "localhost"
+    ]
 
 # Application definition
 
--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -6,7 +6,11 @@ from visits.models import PageVisit
 
 this_dir = pathlib.Path(__file__).resolve().parent
 
-def home_page_view(request, *args, **kwargs):
+def home_view(request, *args, **kwargs):
+    return about_view(request, *args, **kwargs)
+
+
+def about_view(request, *args, **kwargs):
     #queryset = PageVisit.objects.all()
     queryset = PageVisit.objects.filter(path=request.path)
     my_title = "My Page"
@@ -16,13 +20,4 @@ def home_page_view(request, *args, **kwargs):
     }
     html_template = "home.html"
     PageVisit.objects.create(path=request.path)
-    return render(request, html_template, my_context)
-
-
-def about_page_view(request, *args, **kwargs):
-    my_title = "My Page"
-    my_context = {
-        "page_title": my_title
-    }
-    html_template = "about.html"
     return render(request, html_template, my_context)
